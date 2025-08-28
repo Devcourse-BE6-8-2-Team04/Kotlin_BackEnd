17af20c6c2a1c99b63c9318e742cddab441079ec
jiseopshin <wltjq1203@icloud.com>
2025-08-28 10:26:01 +0900

    refactor : dto를 레코드로 변경, 읻따르는 테스트 수정

--- src/main/java/com/team04/back/domain/cloth/cloth/dto/ExtraClothDto.java
+++ src/main/java/com/team04/back/domain/cloth/cloth/dto/ExtraClothDto.java
@@ -2,21 +2,19 @@ package com.team04.back.domain.cloth.cloth.dto;
 
 import com.team04.back.domain.cloth.cloth.entity.ExtraCloth;
 import com.team04.back.domain.weather.weather.enums.Weather;
-import lombok.Getter;
 
-@Getter
-public class ExtraClothDto {
-
-    private int id;
-    private String clothName;
-    private String imageUrl;
-    private Weather weather;
-
-
+public record ExtraClothDto(
+    int id,
+    String clothName,
+    String imageUrl,
+    Weather weather
+) {
     public ExtraClothDto(ExtraCloth extraCloth) {
-        this.id = extraCloth.getId();
-        this.clothName = extraCloth.getClothName();
-        this.imageUrl = extraCloth.getImageUrl();
-        this.weather = extraCloth.getWeather();
+        this(
+            extraCloth.getId(),
+            extraCloth.getClothName(),
+            extraCloth.getImageUrl(),
+            extraCloth.getWeather()
+        );
     }
 }
