717afea1b95a1e3a6af90848fc39679cd4bb3c1c
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-08-29 15:07:03 +0900

    feature: history 집계 데이터 저장 구현

--- src/main/java/com/team04/back/domain/history/history/entity/History.kt
+++ src/main/java/com/team04/back/domain/history/history/entity/History.kt
@@ -1,4 +1,18 @@
-package com.team04.back.domain.history.history.entity;
+package com.team04.back.domain.history.history.entity
 
-public class History {
+import com.team04.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "history")
+class History(
+    var path: String,             // 접근 API 경로 (예: "/review")
+    var userId: String? = null,   // 로그인한 사용자 ID (익명이면 null)
+    var ipAddress: String,        // 사용자 IP
+    var address: String? = null,  // 사용자 주소 (위도, 경도 기준 도시명)
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
 }
+
--- src/main/java/com/team04/back/domain/history/history/repository/HistoryRepository.kt
+++ src/main/java/com/team04/back/domain/history/history/repository/HistoryRepository.kt
@@ -1,4 +1,9 @@
-package com.team04.back.domain.history.history.repository;
+package com.team04.back.domain.history.history.repository
 
-public class HistoryRepository {
+import com.team04.back.domain.history.history.entity.History
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface HistoryRepository : JpaRepository<History, Long>{
+
+
 }
