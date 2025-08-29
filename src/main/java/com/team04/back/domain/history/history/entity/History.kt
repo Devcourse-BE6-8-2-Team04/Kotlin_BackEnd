8f22792ab659db6b924907dd5a97c35722126444
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-08-29 21:47:04 +0900

    refactor: history id 상속

--- src/main/java/com/team04/back/domain/history/history/entity/History.kt
+++ src/main/java/com/team04/back/domain/history/history/entity/History.kt
@@ -1,7 +1,8 @@
 package com.team04.back.domain.history.history.entity
 
 import com.team04.back.global.jpa.entity.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "history")
@@ -10,9 +11,5 @@ class History(
     var userId: String? = null,   // 로그인한 사용자 ID (익명이면 null)
     var ipAddress: String,        // 사용자 IP
     var address: String? = null,  // 사용자 주소 (위도, 경도 기준 도시명)
-) : BaseEntity() {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
-}
+) : BaseEntity()
 
