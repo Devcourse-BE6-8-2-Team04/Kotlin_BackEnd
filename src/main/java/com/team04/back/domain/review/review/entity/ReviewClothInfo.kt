5e89c3835f9d06668579084d92d024744485f4e1
eunchan96 <taegwon02@gmail.com>
2025-08-29 10:22:08 +0900

    feature: ReviewClothInfo 테이블

---
+++ src/main/java/com/team04/back/domain/review/review/entity/ReviewClothInfo.kt
@@ -0,0 +1,25 @@
+package com.team04.back.domain.review.review.entity
+
+import com.team04.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "review_cloth_info")
+class ReviewClothInfo(
+    @field:Column(name = "review_id") val reviewId: Int,
+    @field:Column(name = "cloth_id") val clothId: Int,
+    @field:Column(name = "is_recommend") var isRecommend: Boolean
+) : BaseEntity() {
+    fun toggleRecommend(isRecommend: Boolean): ReviewClothInfo {
+        this.isRecommend = !isRecommend
+        return this
+    }
+
+    companion object {
+        fun of(reviewId: Int, clothId: Int, isRecommend: Boolean): ReviewClothInfo {
+            return ReviewClothInfo(reviewId, clothId, isRecommend)
+        }
+    }
+}
