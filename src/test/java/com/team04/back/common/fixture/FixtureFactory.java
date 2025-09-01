c6b7918895e5e4ffc49abdf20e4a8aaed3ef0109
jiseopshin <wltjq1203@icloud.com>
2025-09-01 11:15:07 +0900

    feat : ClothRecommendationHistory 픽스쳐 생성 메서드 추가

--- src/test/java/com/team04/back/common/fixture/FixtureFactory.java
+++ src/test/java/com/team04/back/common/fixture/FixtureFactory.java
@@ -1,8 +1,11 @@
 package com.team04.back.common.fixture;
 
 import com.team04.back.domain.cloth.cloth.entity.ClothInfo;
+import com.team04.back.domain.cloth.cloth.entity.Clothing;
 import com.team04.back.domain.cloth.cloth.entity.ExtraCloth;
 import com.team04.back.domain.cloth.cloth.enums.Category;
+import com.team04.back.domain.history.history.entity.ClothRecommendationHistory;
+import com.team04.back.domain.user.user.entity.User;
 import com.team04.back.domain.weather.weather.entity.WeatherInfo;
 import com.team04.back.domain.weather.weather.enums.Weather;
 
@@ -58,5 +61,31 @@ public class FixtureFactory {
                     return createWeatherInfo(location, date, weather, (minTemp + maxTemp) / 2);
                 })
                 .collect(Collectors.toList());
+    }
+
+    public static ClothRecommendationHistory createClothRecommendationHistory(
+            User user,
+            String location,
+            List<WeatherInfo> weatherInfos,
+            List<Clothing> likedClothings,
+            List<Clothing> unLikedClothings
+    ) {
+        return new ClothRecommendationHistory(
+                user,
+                location,
+                weatherInfos,
+                likedClothings,
+                unLikedClothings,
+                20.0,   // feelsLike
+                5.0,    // uvi
+                0.1,    // rain
+                0.0,    // snow
+                60,     // humidity
+                3.5,    // windSpeed
+                18.0,   // tempMin
+                27.0,   // tempMax
+                9.0,    // dailyTemperatureGap
+                LocalDateTime.now()
+        );
     }
 }
