32261487decf439faf9d7148692c52e3206772f6
[BE8]권상엽 <tkdduq135790@gmail.com>
2025-09-02 10:30:15 +0900

    user service

---
+++ src/main/java/com/team04/back/domain/user/user/service/UserService.kt
@@ -0,0 +1,18 @@
+package com.team04.back.domain.user.user.service
+
+import com.team04.back.domain.user.user.entity.User
+import com.team04.back.domain.user.user.repository.UserRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: BCryptPasswordEncoder // 빈 등록 필요!
+) {
+    fun register(email: String, password: String, nickname: String): User {
+        require(!userRepository.existsByEmail(email)) { "이미 가입된 이메일입니다." }
+        val encodedPassword = passwordEncoder.encode(password)
+        return userRepository.save(User(email = email, password = encodedPassword, nickname = nickname))
+    }
+}
