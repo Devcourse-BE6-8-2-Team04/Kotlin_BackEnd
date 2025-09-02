b636138a69395c12b7948309dbbc113de3ae2c51
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-09-02 15:56:11 +0900

    refactor: user -> member 도메인 이름 변경

--- src/test/java/com/team04/back/domain/cloth/cloth/service/ClothServiceTest.kt
+++ src/test/java/com/team04/back/domain/cloth/cloth/service/ClothServiceTest.kt
@@ -4,8 +4,9 @@ import com.team04.back.common.fixture.FixtureFactory
 import com.team04.back.domain.cloth.cloth.enums.Category
 import com.team04.back.domain.cloth.cloth.enums.ClothName
 import com.team04.back.domain.cloth.cloth.enums.Style
-import com.team04.back.domain.history.history.entity.ClothRecommendationHistory
-import com.team04.back.domain.user.user.entity.User
+import com.team04.back.domain.member.member.entity.Gender
+import com.team04.back.domain.member.member.entity.Member
+import com.team04.back.domain.member.member.entity.Tendency
 import com.team04.back.domain.weather.weather.entity.WeatherInfo
 import com.team04.back.domain.weather.weather.enums.Weather
 import org.assertj.core.api.Assertions.assertThat
@@ -33,76 +34,17 @@ class ClothServiceTest {
     lateinit var clothRecommendationHistoryRepository: com.team04.back.domain.history.history.repository.ClothRecommendationHistoryRepository
 
     @Test
-    @DisplayName("기간(날씨 정보 리스트)이 주어지면, 각 날씨에 적절한 의류를 추천하여 카테고리별로 반환한다.")
-    fun `get outfit recommendations with weather plan`() {
-        // given
-        val user = User()
-
-        // 1. 날씨 계획
-        val hotWeather = FixtureFactory.createWeatherInfo("Seoul", LocalDate.now().plusDays(1), Weather.CLEAR_SKY, 26.0, 8.0, 0.0, 0.0, 5.0, 7.0)
-        val mildWeather =
-            FixtureFactory.createWeatherInfo("Seoul", LocalDate.now().plusDays(2), Weather.OVERCAST_CLOUDS, 16.0, 5.0, 0.0, 0.0, 3.0, 4.0)
-        val coldWeather =
-            FixtureFactory.createWeatherInfo("Seoul", LocalDate.now().plusDays(3), Weather.HEAVY_RAIN, 4.0, 3.0, 10.0, 0.0, 8.0, 1.0)
-        val weatherPlan = listOf(hotWeather, mildWeather, coldWeather)
-
-        // 2. 의류 생성
-        val summerTee = FixtureFactory.createClothInfo(ClothName.T_SHIRT, Category.TOP, Style.CASUAL_DAILY, 20.0, 30.0)
-        val shorts = FixtureFactory.createClothInfo(ClothName.SHORTS, Category.BOTTOM, Style.CASUAL_DAILY, 20.0, 30.0)
-        val springJacket = FixtureFactory.createClothInfo(ClothName.JACKET, Category.EXTRA, Style.CASUAL_DAILY, 15.0, 25.0)
-        val jeans = FixtureFactory.createClothInfo(ClothName.JEANS, Category.BOTTOM, Style.CASUAL_DAILY, 10.0, 20.0)
-        val winterCoat = FixtureFactory.createClothInfo(ClothName.COAT, Category.EXTRA, Style.CASUAL_DAILY, 0.0, 10.0)
-        val scarf = FixtureFactory.createClothInfo(ClothName.SCARF, Category.EXTRA, Style.OUTDOOR, 0.0, 10.0)
-        val formalShirt = FixtureFactory.createClothInfo(ClothName.SHIRT, Category.TOP, Style.FORMAL_OFFICE, 10.0, 20.0)
-        val runningShoes = FixtureFactory.createClothInfo(ClothName.ATHLETIC_SHOES, Category.SHOES, Style.OUTDOOR, 18.0, 28.0)
-
-        // 3. 과거 추천 기록 mocking
-        val hotHistory: ClothRecommendationHistory = FixtureFactory.createClothRecommendationHistory(
-            user,
-            "Seoul",
-            listOf(hotWeather),
-            listOf(summerTee, shorts, runningShoes),
-            emptyList()
-        )
-
-        val mildHistory: ClothRecommendationHistory = FixtureFactory.createClothRecommendationHistory(
-            user,
-            "Seoul",
-            listOf(mildWeather),
-            listOf(springJacket, jeans, formalShirt),
-            emptyList()
-        )
-
-        val coldHistory: ClothRecommendationHistory = FixtureFactory.createClothRecommendationHistory(
-            user,
-            "Seoul",
-            listOf(coldWeather),
-            listOf(winterCoat, scarf),
-            emptyList()
-        )
-
-        whenever(clothRecommendationHistoryRepository.findByDateAndLocation(hotWeather.date, "Seoul"))
-            .thenReturn(listOf(hotHistory))
-        whenever(clothRecommendationHistoryRepository.findByDateAndLocation(mildWeather.date, "Seoul"))
-            .thenReturn(listOf(mildHistory))
-        whenever(clothRecommendationHistoryRepository.findByDateAndLocation(coldWeather.date, "Seoul"))
-            .thenReturn(listOf(coldHistory))
-
-        // 4. 서비스 호출
-        val result = clothService.getOutfitRecommendations(weatherPlan, "Seoul")
-
-        // 5. 검증
-        assertThat(result.recommendedOutfits[Category.TOP]).containsExactlyInAnyOrder(summerTee, formalShirt)
-        assertThat(result.recommendedOutfits[Category.BOTTOM]).containsExactlyInAnyOrder(shorts, jeans)
-        assertThat(result.recommendedOutfits[Category.EXTRA]).containsExactlyInAnyOrder(springJacket, winterCoat, scarf)
-        assertThat(result.recommendedOutfits[Category.SHOES]).containsExactlyInAnyOrder(runningShoes)
-    }
-
-    @Test
     @DisplayName("다양한 날씨와 30개 이상의 과거 추천 기록이 있을 때, 기간별 옷차림을 정확히 추천한다.")
     fun `test outfit recommendations with extensive history and edge cases`() {
         // given
-        val user = User()
+        val member = Member(
+            userId = "testUser",
+            password = "testPassword",
+            email = "test@example.com",
+            age = 30,
+            gender = Gender.MALE,
+            tendency = Tendency.NEUTRAL
+        )
         val location = "Busan"
         val startDate = LocalDate.now()
         val endDate = startDate.plusDays(29) // 30 days
@@ -149,7 +91,7 @@ class ClothServiceTest {
             allCloths.shuffled().take(2).forEach { notRecommendedCloths.add(it) }
 
             val history = FixtureFactory.createClothRecommendationHistory(
-                user,
+                member,
                 location,
                 listOf(weatherInfo),
                 recommendedCloths,
